df58f564cac4694fc217bcca801843935f2871e7
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-19 11:24:54 -0300

    maximize 14

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ function App() {
     if (height === window.innerHeight*0.75) {
       setHeight(maxHeight);
       setWidth(maxWidth);
-      setWindowPosition({ x: 769, y: 330});
+      setWindowPosition({ x: 769, y: 375});
     } else {
       setHeight(initialHeight);
       setWidth(initialWidth);
