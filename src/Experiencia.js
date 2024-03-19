f06cb14f0a4e825a5073e1dcd8f6bc49257421af
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-19 16:08:44 -0300

    content 15

--- src/Experiencia.js
+++ src/Experiencia.js
@@ -4,17 +4,19 @@ function Experiencia({ name, textColor }) {
     return(
         <div className='plain-text'>
             <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b>Experiencia Laboral:</p>
-            <p></p>
-            <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>MAR 2020 - MAR 2021: Banco República(brou)</p>
-            <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Rol: Pasante</p>
+            <p className='ocultar'>Enter</p>
+            <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>MAR 2020 - MAR 2021:</p>
+            <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Banco República(brou)</p>
+            <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Rol:</p>
+            <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Pasante</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Tareas Desempeñadas:</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Gestión de inventario</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Atención al público</p>           
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Asistir a clientes en tramites bancarios(remoto y presencial)</p>   
-            <p></p>
-            <p></p>
+            <p className='ocultar'>Enter</p>
+            <p className='ocultar'>Enter</p>
             <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b>Habilidades:</p>
-            <p></p>
+            <p className='ocultar'>Enter</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>HTML</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>CSS</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>JavaScript</p>
