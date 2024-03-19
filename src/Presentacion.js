363326aa306f0af6e608012ee2e54aad1cd086a8
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-19 13:43:23 -0300

    content 4

--- src/Presentacion.js
+++ src/Presentacion.js
@@ -4,7 +4,7 @@ function Presentacion({ name, textColor }) {
     return(
         <div className='plain-text'>
             <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b>Sobre mi:</p>
-            <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Soy un profesional motivado en el desarrollo frontend, con sólida formación en Ingeniería en Computación.</p>
+            <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Soy desarrollador frontend, con sólida formación en Ingeniería en Computación.</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Mi compromiso es aportar al equipo mis habilidades técnicas y mi entusiasmo por el aprendizaje continuo.</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Con dominio en desarrollo frontend y en proceso de dominar ReactJs,</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>estoy preparado para contribuir al éxito de sus proyectos con responsabilidad y dedicación.</p>
