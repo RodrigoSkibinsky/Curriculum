6895455ac6fe999257938ab13f089632acf324de
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-19 16:19:59 -0300

    content 16

--- src/Experiencia.js
+++ src/Experiencia.js
@@ -4,7 +4,7 @@ function Experiencia({ name, textColor }) {
     return(
         <div className='plain-text'>
             <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b>Experiencia Laboral:</p>
-            <p className='ocultar'>Enter</p>
+            <p className='oculto'>Enter</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>MAR 2020 - MAR 2021:</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Banco República(brou)</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Rol:</p>
@@ -13,10 +13,10 @@ function Experiencia({ name, textColor }) {
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Gestión de inventario</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Atención al público</p>           
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Asistir a clientes en tramites bancarios(remoto y presencial)</p>   
-            <p className='ocultar'>Enter</p>
-            <p className='ocultar'>Enter</p>
+            <p className='oculto'>Enter</p>
+            <p className='oculto'>Enter</p>
             <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b>Habilidades:</p>
-            <p className='ocultar'>Enter</p>
+            <p className='oculto'>Enter</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>HTML</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>CSS</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>JavaScript</p>
