f03eda69acda999cc69b3820b53026eab3b2737a
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-20 09:14:12 -0300

    screen mod 2

--- src/Screen.js
+++ src/Screen.js
@@ -28,6 +28,55 @@ function Screen() {
     const initialX = (screenWidth - windowWidth) / 2;
     const initialY = (screenHeight - windowHeight) / 2;
 
+    
+
+function getTerminalName(image) {
+    switch (image) {
+      case terminalDefault:
+        return "Terminal Emulator";
+      case terminalRoot:
+        return "Root Terminal Emulator";
+      case terminalUser:
+        return "PowerShell";
+      default:
+        return "";
+    }
+  }
+  
+  function getTerminalTitle(image) {
+    switch (image) {
+      case terminalDefault:
+        return "kali@kali:~";
+      case terminalRoot:
+        return "root@kali:~";
+      case terminalUser:
+        return "PS> kali@kali:home/kali";
+      default:
+        return "";
+    }
+  }
+  
+  function getTerminalSubTitle(image) {
+    switch (image) {
+      case terminalDefault:
+        return "kali@kali:~ ";
+      case terminalRoot:
+        return "root@kali:# ";
+      case terminalUser:
+        return "kali@kali:PS> ";
+      default:
+        return "";
+    }
+  }
+  
+  function getTerminalColor(image) {
+    if (image === terminalRoot) {
+      return "#e44";
+    } else {
+      return "#3060ff";
+    }
+  }
+
     const closeScreen = () => {//Solo se usa dentro de screen
       setIsScreenHidden(true);
       setIsScreenClosed(true);
