6f301b9c01e359cd8b752ad752f68d68f1b550c9
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-26 18:13:17 -0300

    terminal estudios

--- src/Estudios.js
+++ src/Estudios.js
@@ -7,6 +7,7 @@ function Estudios({ name, textColor }) {
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Estudios Terciarios:</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>MAR 2021 - Actualidad: Ingeniería en Computación - UDELAR</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Escolaridad disponible <a href='Escolaridad1.pdf' target='_blank'>aquí</a></p>
+            <p className='oculto'>Enter</p>
             <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b>Idiomas:</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Inglés - Nivel B2:</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Portugués - Nivel Intermedio</p>
