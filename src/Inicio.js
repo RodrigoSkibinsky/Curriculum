624da659ff2db380a10d4ef3d47982c5d8d51e42
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-30 17:44:54 -0300

    InicioAgregadoConExplicacion2

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
-function Estudios({ name, textColor }) {
+function Inicio({ name, textColor }) {
     return(
         <div className='plain-text'>
             <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b>Bienvenido/a a mi Curriculum Online</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Mi nombre es Rodrigo Sacramento  y soy un Desarrollador FrontEnd en proceso de perfeccionar mis habilidades de BackEnd</p>
             <p className='terminal-text'><b style={{color: textColor,}}>{name}</b>Siéntase libre de explorar esta página y sus funciones ocultas.</p>
             <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b>Puede acceder a mi información a través de las opciones de la terminal o sino mediante el menu de inicio en la esquina superior izquierda.</p>
+            <p className='terminal-sub-title'><b style={{color: textColor,}}>{name}</b><p style={{color:'red',}}>Advertencia: por el momento esta pagina no esta optimizada para dispositivos móviles o touch-screen.<p/></p>
         </div>
     );
 }
