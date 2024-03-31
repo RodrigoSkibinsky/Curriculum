338aac0842148e4d35793f81f8aca8bfce2fc401
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-31 16:59:09 -0300

    terminalTopAgregadoCompleto

--- src/App.js
+++ src/App.js
@@ -336,46 +336,8 @@ function App() {
             left: windowPosition.x + 'px',
             height: height,
             width: width,
-          }} >
-          {/* <div className="top" 
-            style={{
-             cursor: dragging ? 'all-scroll' : 'default' 
-            }} onMouseDown={handleMouseDown}
-          >
-            <div className='top-top'>
-              <div className='top-space top-left'>
-              <img src={terminalDefault} className="app-small" alt="logo" style={{
-                marginLeft: '10px',
-              }}/>
-              </div>
-              <div className='top-space top-middle'>
-                <p>{getTerminalTitle(selection)}</p>
-              </div>
-              <div className='top-space top-right'>
-                <div className='button minimize' onClick={minimize}>
-                  -
-                </div>
-                <div className='button maximize' onClick={maximize}>
-                  <div className='maximize-square'>
-                  </div>
-                </div>
-                <div className='button close' onClick={closeTerminal}>
-                  x
-                </div>
-              </div>
-            </div>
-            <div className='top-bottom'>
-              <div className='top-space top-left'>
-                <p className='terminal-item' onClick={() => handleTerminalItemClick("File")}>File</p>
-                <p className='terminal-item' onClick={() => handleTerminalItemClick("Action")}>Action</p>
-                <p className='terminal-item' onClick={() => handleTerminalItemClick("Edit")}>Edit</p>
-                <p className='terminal-item' onClick={() => handleTerminalItemClick("View")}>View</p>
-                <p className='terminal-item' onClick={() => handleTerminalItemClick("Help")}>Help</p>
-              </div>
-              <div className='top-space top-middle'></div>
-              <div className='top-space top-right'></div>
-            </div>
-          </div> */}
+          }}
+        >
           <TerminalTop dragging={dragging} handleMouseDown={handleMouseDown} handleTerminalItemClick={handleTerminalItemClick} minimize={minimize} maximize={maximize} closeTerminal={closeTerminal} terminalTitle={getTerminalTitle(selection)} />
           <div className="line"></div>
           <TerminalContent selected={selection} altura={height} textoTerminal={terminalText}/>
