51e15e661047e793700b3fd867d0cb07b5841d3b
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-31 23:06:06 -0300

    CambioEstructural8

--- src/Terminal/TerminalTop.js
+++ src/Terminal/TerminalTop.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './../App.css';
 
-const terminalDefault = './../img/terminal-default-icon.png'
+import terminalDefault from './terminal-default-icon.png'
 
 function TerminalTop({ dragging, handleTerminalItemClick, handleMouseDown, minimize, maximize, closeTerminal, terminalTitle }) {
   
