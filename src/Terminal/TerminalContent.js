690704ec9a08d69910654a65199dad4e3ff209d6
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-03-31 22:36:47 -0300

    CambioEstructural4

--- src/Terminal/TerminalContent.js
+++ src/Terminal/TerminalContent.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo-kali.svg';
 import fotoPerfil from './foto-perfil-cv.jpg';
 import './../App.css';
 import Clock from '../Clock.js';
