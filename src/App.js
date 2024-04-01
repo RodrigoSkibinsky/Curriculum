93a5648ff212b362140f02e34e1e6233db2f3972
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-04-01 12:34:02 -0300

    plusTest

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 import Desktop from './Desktop.js';
 
 function App() {
+  const [isTouchScreen, setIsTouchScreen] = useState(false);
+  const OSInfo = () => {
   
+    useEffect(() => {
+      const isTouchDevice = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
+      setIsTouchScreen(isTouchDevice);
+    }, []);
+  }
   return (
     <div className="App-Holder">
       <Desktop />
+      <div>
+        <p style={{color:'#000', textAlign: 'center'}}>¿Dispositivo táctil?: {isTouchScreen ? 'Sí' : 'No'}</p>
+      </div>
     </div>
   );
 }
