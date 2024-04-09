6fd9959c1c8b1609f6c0f9993d272bb0f0ee4a33
Rodrigo Sacramento Skibinsky <rodrigo.sacramento@fing.edu.uy>
2024-04-09 15:53:40 -0300

    prueba7

--- src/BarraTareasMobile.js
+++ src/BarraTareasMobile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo-kali.svg';
 import Proyectos from './proyectos.png';
 import terminalDefault from './terminal-default-icon.png';
